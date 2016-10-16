b4595ab4b61d3a5d5fa9e9b9b32d5461ef71b0e0
psgb2811 <psgb2811@gmail.com>
2016-10-16 20:43:21 +0530

    Update main.c

--- Source/main.c
+++ Source/main.c
@@ -577,6 +577,7 @@ void mtp_start() {
 				
 				printf("Received a non-Bcast frame\n");
 				populate_HAT(recvOnEtherPort); //Function added by Guru
+				populate_HAT(recvOnEtherPort); //Function added by Guru
 				/*
 				printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) &eheader->ether_shost));
 				printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *)&eheader->ether_dhost));
