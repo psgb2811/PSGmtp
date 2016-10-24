74863af6dccd7789e6859ce8e129239af3081668
psgb2811 <psgb2811@gmail.com>
2016-10-25 00:33:50 +0530

    Update main.c

--- Source/main.c
+++ Source/main.c
@@ -276,6 +276,8 @@ time(&conv_beg);
 				print_entries_cpvid_LL();               // CHILD PVID TABLE
 				print_entries_lbcast_LL();              // LOCAL HOST PORTS
 				printf("\n\n\n");
+					printf("Code executed in %f milliseconds.",(double)(difftime(conv_fin, conv_beg)));
+
 
 			}
 				
